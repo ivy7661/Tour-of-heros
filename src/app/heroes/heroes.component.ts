0f32aa138367cafaebdfb933cb0015dc267db958
Ivy <ivy7661@gmail.com>
2024-06-11 23:32:11 +0800

    add pipe

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.scss'
 })
 export class HeroesComponent {
-  hero='Windstorm'
+  hero:Hero= {
+    id:1,
+    name:'WindStorm'
+  }
 }
